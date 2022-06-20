6aa2020e59286db8af7533a71c1c7cbdd02d6cf4
kuba <jakub.g.grudzinski@gmail.com>
2022-06-20 18:36:43 +0200

    ćwiczenia

--- cod.py
+++ cod.py
@@ -1,2 +1,11 @@
-while True:
-    pass
+# podana_wartosc = input("tutaj jest komunikat, ktory jest wyswietlany uzytkownikowi przed podaniem wartosci") # podaj string
+# podana_wartosc = str(input("podaj imię\n"))
+licznik = int(input("podaj ilość pętli\n"))
+# print(podana_wartosc)
+# wygrana=False
+while licznik > 0:
+    print("jeszcze nie wygrales")
+    # wygrana = True # <- usun '#' i zobacz co sie staniekuba
+    print(f"licznik: {licznik}")
+    licznik = licznik - 1
+print("koniec gry")
