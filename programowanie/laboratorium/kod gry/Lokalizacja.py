ee507e665b7c2e58a4d040b52689d0f39cd8a0ee
ubuntu <Krzychu.rychu04@gmail.com>
2022-07-13 13:09:15 +0200

    Lokalizacja

--- programowanie/laboratorium/kod gry/Lokalizacja.py
+++ programowanie/laboratorium/kod gry/Lokalizacja.py
@@ -3,13 +3,25 @@ class Lokalizacja:
         self._x = x
         self._y = y
     
-    def aktualizuj(self):
-        if event.key == pygame.K_LEFT:
-            self.left_pressed = True
-        if event.key == pygame.K_RIGHT:
-            self.right_pressed = True
-        if event.key == pygame.K_UP:
-            self.up_pressed = True
-        if event.key == pygame.K_DOWN:
-            self.down_pressed = True
+    def aktualizuj(self, key, type):
+        
+        if type == "pygame.KEYDOWN":
+            if key == pygame.K_LEFT:
+                self.left_pressed = True
+            if key == pygame.K_RIGHT:
+                self.right_pressed = True
+            if key == pygame.K_UP:
+                self.up_pressed = True
+            if key == pygame.K_DOWN:
+                self.down_pressed = True
+                
+        if type == "pygame.KEYUP":
+            if event.key == pygame.K_LEFT:
+                self.left_pressed = False
+            if event.key == pygame.K_RIGHT:
+                self.right_pressed = False
+            if event.key == pygame.K_UP:
+                self.up_pressed = False
+            if event.key == pygame.K_DOWN:
+                self.down_pressed = False
 
