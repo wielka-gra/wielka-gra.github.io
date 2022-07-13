ac2ac7d75d8994a2292308097577a555d90e8b80
Jakub-Ner <kubaner1@gmail.com>
2022-07-13 12:51:28 +0200

    walka

--- cod.py
+++ cod.py
@@ -1,13 +1,20 @@
-# podana_wartosc = input("tutaj jest komunikat, ktory jest wyswietlany uzytkownikowi przed podaniem wartosci") # podaj string
-# podana_wartosc = str(input("podaj imię\n"))
+import random
 
 
 class atakMieczem:
+    def losowy_dmg(self):
+        dmg = random.randint(0, 5)
+        return dmg
+
     def __repr__(self) -> str:
         return "atak mieczem"
 
 
 class atakpiescia:
+    def losowy_dmg(self):
+        dmg = random.randint(0, 5)
+        return dmg
+
     def __repr__(self) -> str:
         return "atak piescią"
 
@@ -23,6 +30,9 @@ class blokMiczem:
 
 
 class atakNoga:
+    def losowy_dmg(self):
+        dmg = random.randint(0, 5)
+        return dmg
 
     def __repr__(self) -> str:
         return "atak noga"
@@ -30,17 +40,42 @@ class atakNoga:
 
 ataki = [atakpiescia(), atakNoga(), atakMieczem(), blokMiczem(), unik()]
 
-hp_b = 100
-hp_p = 120
+hp_b = 50
+hp_p = 20
 
-while hp_b >= 0 and hp_p >= 0:
-    wybor = int(input("czym chcesz uderzyc albo czym chcesz sie obronic\n" + str(ataki) + "\n"))
+
+def zadawanie_obrazen(hp, dmg):
+    print("obrarzenia", dmg)
+    hp = hp - dmg
+    print("zdrowie", hp)
+    return hp
+
+
+while hp_b > 0 and hp_p > 0:
+    wybor = 5
+    while wybor > 4:
+        wybor = int(input("czym chcesz uderzyc albo czym chcesz sie obronic\n" + str(ataki) + "\n"))
     atak = ataki[wybor]
     print(atak)
     if wybor < 3:
-        hp_p -= 50
+        print("bohater:")
+        dmg = atak.losowy_dmg()
+        hp_p = zadawanie_obrazen(hp_p, dmg)
+        # teraz misiek nas leje
+        szansa_na_uderzenie = random.randint(0, 100)
 
-print("koniec gry")
+        #niedzwiedz nas bije
+        if szansa_na_uderzenie > 50:
+            print("\nPotwor:")
+            hp_b = zadawanie_obrazen(hp_b, 10)
+    else:
+        pass
+
+if hp_b <= 0:
+    print("przegrales")
+if hp_p <= 0:
+    print("wygrales")
+
 
 if __name__ == '__main__':
     pass
